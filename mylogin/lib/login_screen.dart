9d60473c0e9e7f87127537111a3f7d7f08d372c3
IrnandaNanda <irnandatri1509@gmail.com>
2025-11-25 14:07:58 +0700

    Menyelesaikan Login Page

--- mylogin/lib/login_screen.dart
+++ mylogin/lib/login_screen.dart
@@ -1,30 +1,82 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+
+class LoginScreen extends StatelessWidget{
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build (BuildContext context){
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Login Untuk Belajar dengan Agus'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Padding(padding: const EdgeInsets.only(top: 60.0),
-          child: Container(
-            width: 250,
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(50.0)
+        title: Text('login Page')
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(padding: const EdgeInsets.only(top: 60.0),
+            child: Center(
+              child: Container(
+                width: 200,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(50.0)            
+                ),
+                child: Image.asset('assets/stikma_logo.png'),
+              ),
+            ),),
+            Padding(padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0, bottom: 0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+                hintText: 'Enter your Email'),
+            ),),
+            Padding(padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0, bottom: 0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'password',
+                hintText: 'Enter your password'),
+            ),),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                'forget password',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
             ),
-          child: Image.asset('assets/images/login.png'),
-          ),
-          )
-        ]
-      )
+            SizedBox(height: 20),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => Scaffold(
+                        appBar: AppBar(title: Text('Home')),
+                        body: Center(child: Text('Home Page')),
+                      ),
+                    ),
+                  );
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ),
+          ],
+        ),
     );
   }
 }
