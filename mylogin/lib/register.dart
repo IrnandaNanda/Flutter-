f6491b3cef7960910432a9764c24ade396263b8a
IrnandaNanda <irnandatri1509@gmail.com>
2025-12-02 14:46:12 +0700

    Membuat Register Page

---
+++ mylogin/lib/register.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  bool pasVisible = false;
+  bool pasConfirmVisible = false;
+
+  void togglePasswordView() {
+    setState(() {
+      pasVisible = !pasVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Register'),
+        ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(18.0, 8.0, 18.0, 8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                  suffixIcon: const Icon(
+                    Icons.person,
+                    ), 
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(18.0, 8.0, 18.0, 8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                  suffixIcon: const Icon(
+                    Icons.key,
+                  ),  
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue.shade600,
+                    foregroundColor: Colors.white,
+                    shadowColor: Colors.blue.shade900,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  ),
+                  child: const Text(
+                    'Save',
+                    style: TextStyle(fontSize: 18),
+                    ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue.shade600,
+                    foregroundColor: Colors.white,
+                    shadowColor: Colors.blue.shade900,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  ),
+                  child: const Text(
+                    'Cancel',
+                    style: TextStyle(fontSize: 18),
+                    ),
+                )
+              ],
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
