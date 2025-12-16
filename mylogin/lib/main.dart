71f4907eb861e5496bb6400207fa05e550a6ffff
IrnandaNanda <irnandatri1509@gmail.com>
2025-12-16 07:58:10 +0700

    Update Main

--- mylogin/lib/main.dart
+++ mylogin/lib/main.dart
@@ -10,6 +10,11 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
       home: LoginScreen(),
     );
   }
